a038e40cf7e6b616a5cc01a780fd43ba549cf3c1
SeoEunGi0802 <lolove08@naver.com>
2021-09-07 15:57:43 +0900

    0907 commit
    중간커밋 : 코드 전명 수정 후

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.listen(3000);
 
-const users = [{
+let users = [{
     id: 1,
     name: "길동1",
     age: 21
@@ -37,57 +37,89 @@ let user;
 let result;
 
 app.get("/users", (req, res) => {
-    res.send(user);
+    res.send({
+        count: users.length,
+        users
+    });
 });
 
 app.get("/users/:id", (req, res) => {
-    const result = _.find(users, { id: parseInt(req.params.id) });
-    res.send("user " + req.params.id + " get");
+    const find_user = _.find(users, { id: parseInt(req.params.id) });
+
+    if (find_user) {
+        result = `정상적으로 조회되었습니다.`
+
+        res.status(200).send({
+            result
+        });
+    } else {
+        result = `해당 아이디를 가진 유저가 없습니다.`
+
+        res.status(400).send({
+            result
+        });
+    }
 });
 
 // user 생성
 app.post("/users", (req, res) => {
     const create_user = req.body
+    const check_user = _.find(users, ["id", create_user.id]);
 
-    if (create_user.id && create_user.name && create_user.age) {
-        // users.push(user);
-        user = create_user;
-        result = `${user.name}님을 생성 했습니다.`
+    if (!check_user && create_user.id && create_user.name && create_user.age) {
+        users.push(create_user);
+        result = `${create_user.name}님을 생성 했습니다.`
+
+        res.status(200).send({
+            result
+        });
     } else {
         result = `입력 요청값이 잘못되었습니다.`
-    }
 
-    res.send({
-        result
-    });
+        res.status(400).send({
+            result
+        });
+    }
 });
 
 // name 변경
 app.put("/users/:id", (req, res) => {
-    if (user && user.id == req.params.id) {
-        user.name = req.body.name
-        result = `유저 이름을 ${user.name}으로 변경하였습니다.`
-    } else {
-        result = `아이디가 ${req.params.id}인 유저가 존재하지 않습니다.`
-    }
+    const find_user_idx = _.findIndex(users, ["id", parseInt(req.params.id)]);
 
-    res.send({
-        result
-    });
-});
+    if (find_user_idx !== -1) {
+        users[find_user_idx].name = req.body.name;
 
-//user 지우기
+        result = `성공적으로 수정 되었습니다.`;
 
-app.delete("/users/:id", (req, res) => {
-    if (user && user.id == req.params.id) {
-        user = null;
-        result = `아이디가 ${req.params.id}인 유저 삭제`;
+        res.status(200).send({
+            result
+        });
     } else {
         let result = `아이디가 ${req.params.id}인 유저가 존재하지 않습니다.`;
+
+        res.status(400).send({
+            result
+        });
     }
+});
 
-    res.send({
-        result
-    });
+// user 지우기
+app.delete("/users/:id", (req, res) => {
+    const check_user = _.find(users, ["id", parseInt(req.params.id)]);
 
+    if (check_user) {
+        users = _.reject(users, ["id", parseInt(req.params.id)]);
+
+        result = `성공적으로 삭제 되었습니다.`;
+
+        res.status(200).send({
+            result
+        });
+    } else {
+        let result = `아이디가 ${req.params.id}인 유저가 존재하지 않습니다.`;
+
+        res.status(400).send({
+            result
+        });
+    }
 });
